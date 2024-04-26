1d69f2b99bdc27de38a1e222cfd1d2d1809c9469
Joe Osborne <joerosborne@gmail.com>
2024-04-25 21:10:46 -0700

    simplifying some files

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,14 @@
 echo "command - npx tsc"
 npx tsc
 
-echo "command - aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <account-id>.dkr.ecr.<region>.amazonaws.com"
-aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <account-id>.dkr.ecr.<region>.amazonaws.com
+echo "command - aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <aws-account-id>.dkr.ecr.<region>.amazonaws.com"
+aws ecr get-login-password --region <region> | docker login --username AWS --password-stdin <aws-account-id>.dkr.ecr.<region>.amazonaws.com
 
-echo "command - docker build --platform linux/amd64 --progress=plain -t custom-scraping ."
-docker build --platform linux/amd64 --progress=plain -t custom-scraping .
+echo "command - docker build --platform linux/amd64 --progress=plain -t scraper ."
+docker build --platform linux/amd64 --progress=plain -t scraper .
 
-echo "command - docker tag custom-scraping:latest <account-id>.dkr.ecr.<region>.amazonaws.com/custom-scraping:latest"
-docker tag custom-scraping:latest <account-id>.dkr.ecr.<region>.amazonaws.com/custom-scraping:latest
+echo "command - docker tag scraper:latest <aws-account-id>.dkr.ecr.<region>.amazonaws.com/scraper:latest"
+docker tag scraper:latest <aws-account-id>.dkr.ecr.<region>.amazonaws.com/scraper:latest
 
-echo "command - docker push <account-id>.dkr.ecr.<region>.amazonaws.com/custom-scraping:latest"
-docker push <account-id>.dkr.ecr.<region>.amazonaws.com/custom-scraping:latest
+echo "command - docker push <aws-account-id>.dkr.ecr.<region>.amazonaws.com/scraper:latest"
+docker push <aws-account-id>.dkr.ecr.<region>.amazonaws.com/scraper:latest
